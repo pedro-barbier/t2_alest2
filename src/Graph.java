afb0da8cf3a36abefca1ecfddb48872fa8e04ceb
barbier1 <pedro.barbieri@edu.pucrs.br>
2025-06-20 14:40:37 -0300

    Remoção de código desnecessário

--- src/Graph.java
+++ src/Graph.java
@@ -35,7 +35,6 @@
  ******************************************************************************/
 
 import java.util.HashSet;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
@@ -84,61 +83,6 @@ public class Graph {
         for (int v = 0; v < V; v++) {
             adj[v] = new Bag<Integer>();            
         }        
-    }
-
-    /**  
-     * Initializes a graph from the specified input stream.
-     * The format is the number of vertices <em>V</em>,
-     * followed by the number of edges <em>E</em>,
-     * followed by <em>E</em> pairs of vertices, with each entry separated by whitespace.
-     *
-     * @param  in the input stream
-     * @throws IllegalArgumentException if the endpoints of any edge are not in prescribed range
-     * @throws IllegalArgumentException if the number of vertices or edges is negative
-     * @throws IllegalArgumentException if the input stream is in the wrong format
-     */
-    public Graph(In in) {
-        try {
-            this.V = in.readInt();
-            if (V < 0) throw new IllegalArgumentException("number of vertices in a Graph must be nonnegative");
-            adj = (Bag<Integer>[]) new Bag[V];
-            for (int v = 0; v < V; v++) {
-                adj[v] = new Bag<Integer>();
-            }
-            int E = in.readInt();
-            if (E < 0) throw new IllegalArgumentException("number of edges in a Graph must be nonnegative");
-            for (int i = 0; i < E; i++) {
-                int v = in.readInt();
-                int w = in.readInt();
-                validateVertex(v);
-                validateVertex(w);
-                addEdge(v, w); 
-            }
-        }
-        catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("invalid input format in Graph constructor", e);
-        }
-    }
-
-
-    /**
-     * Initializes a new graph that is a deep copy of {@code G}.
-     *
-     * @param  G the graph to copy
-     */
-    public Graph(Graph G) {
-        this(G.V());
-        this.E = G.E();
-        for (int v = 0; v < G.V(); v++) {
-            // reverse so that adjacency list is in same order as original
-            Stack<Integer> reverse = new Stack<Integer>();
-            for (int w : G.adj[v]) {
-                reverse.push(w);
-            }
-            for (int w : reverse) {
-                adj[v].add(w);
-            }
-        }
     }
 
     /**
@@ -248,20 +192,6 @@ public class Graph {
         }
         s.append("}");
         return s.toString();
-    }
-
-
-    /**
-     * Unit tests the {@code Graph} data type.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        Graph G = new Graph(in);
-        StdOut.println(G);
-        StdOut.println();
-        StdOut.println(G.toDot());
     }
 
 }
